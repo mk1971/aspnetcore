265adc660b87b02a313c093950f611762019d1b4
mk1971 <wagner.maki@hotmail.com>
2020-04-21 10:07:14 -0300

    Acesso a mais do que um ViewModel da mesma View

---
+++ ASP6/ASP6/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP6.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //criar um homem
+            Humano homem = new Humano()
+            {
+                Nome = "João",
+                Apelido = "Riberiro"
+            };
+
+            //criar carro
+            Automovel carro = new Automovel()
+            {
+                Marca = "Audi",
+                Cor = "Metal"
+            };
+
+
+            //coloca os 2 viewmodels no mesmo BAG (um viewmodel geral)
+            BAG saco = new BAG()
+            {
+                humano = homem,
+                automovel = carro
+            };
+
+            return View(saco);
+        }
+    }
+}
---
+++ ASP6/ASP6/ViewModels/ViewModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP6.ViewModels
+{
+    public class Humano
+    {
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+    }
+
+    public class Automovel
+    {
+        public string Marca { get; set; }
+        public string Cor { get; set; }
+    }
+
+    public class BAG
+    {
+        public Humano humano { get; set; }
+        public Automovel automovel { get; set; }
+    }
+}
