fb052e0dbf2098c3a4d4d6b0642d20375f45f676
mk1971 <wagner.maki@hotmail.com>
2020-04-21 09:52:40 -0300

    Utilização de ViewModel

---
+++ Asp2/Asp2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp2.ViewModels;
+
+namespace Asp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Cliente novo = new Cliente()
+            {
+                nome = "João",
+                apelido = "Ribeiro",
+                telefone = "123456789"
+            };
+
+            return View(novo);
+        }
+    }
+}
---
+++ Asp2/Asp2/ViewModels/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp2.ViewModels
+{
+    public class Cliente
+    {
+        public string nome { get; set; }
+        public string apelido { get; set; }
+        public string telefone { get; set; }
+
+    }
+}
