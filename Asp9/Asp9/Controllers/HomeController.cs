62a154b3a7ec9169850f724e821a5ddecd9c0ce4
mk1971 <wagner.maki@hotmail.com>
2020-04-21 14:55:05 -0300

    teste sem debug e Attribute Routing

---
+++ Asp9/Asp9/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp9.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        [Route("Home/aaa")]
+        public string Index()
+        {
+            return "Estou no Index";
+        }
+
+        [Route("Home/acao1/{id?}")]
+        public string acao1(int id)
+        {
+            return "Estou na acao1. ID = " + id;
+        }
+
+        [Route("home/post")]
+        public string acao2()
+        {
+            return "Estou na acao2. Novo";
+        }
+    }
+}
