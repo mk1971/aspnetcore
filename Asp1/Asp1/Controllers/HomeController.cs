0162dd78cd7f09e9d55e155399a1692eddc47deb
mk1971 <wagner.maki@hotmail.com>
2020-04-21 09:50:00 -0300

    Criando um projeto MVC a partir de projeto vazio

---
+++ Asp1/Asp1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ViewData]
+        public string nome { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult pag1()
+        {
+            //utilizar o ViewBag
+            //ViewBag.mensagem1 = "Esta é a mensagem 1 para a página 1.";
+            //ViewBag.mensagem2 = "Esta é a mensagem 2 para a página 1.";
+            
+            return View();
+        }
+
+        public IActionResult pag2()
+        {
+
+            //usando ViewData
+            //ViewData["nome"] = "João";
+            //ViewData["apelido"] = "Ribeiro";
+
+            return View();
+        }
+
+        public IActionResult pag3()
+        {
+            nome = "João";
+            return View();
+        }
+
+    }
+}
