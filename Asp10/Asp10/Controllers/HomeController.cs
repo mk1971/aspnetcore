082fecd87bfd051b1918d884c5085e520fd94ba1
mk1971 <wagner.maki@hotmail.com>
2020-04-22 14:27:53 -0300

    Mais informacoes sobre Attribute Routing

---
+++ Asp10/Asp10/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Estou no Index";
+        }
+
+        [Route("home/adicionar/{parcela1?}/{parcela2?}")]
+        public string Adicionar(int parcela1, int parcela2)
+        {
+            return parcela1 + " + " + parcela2 + " = " + (parcela1 + parcela2).ToString();
+        }
+    }
+}
