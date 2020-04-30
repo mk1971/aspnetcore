e6f92a6431ec23a8e8719ea15cb8e716b2750af3
mk1971 <wagner.maki@hotmail.com>
2020-04-30 11:56:41 -0300

    Um olhar introdutorio sobre os Models

---
+++ Asp18/Asp18/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp18.Models;
+
+namespace Asp18.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Cliente> lista_clientes = new List<Cliente>();
+            lista_clientes.Add(new Cliente() { nome = "João", apelido = "Ribeiro" });
+            lista_clientes.Add(new Cliente() { nome = "Ana", apelido = "Silva" });
+            lista_clientes.Add(new Cliente() { nome = "Carlos", apelido = "Oliveira" });
+
+            return View(lista_clientes);
+        }
+    }
+}
