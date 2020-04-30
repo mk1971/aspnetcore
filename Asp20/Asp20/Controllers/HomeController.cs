97a80b71fbf51ea257a99790e213189592e7ecbc
mk1971 <wagner.maki@hotmail.com>
2020-04-30 16:32:20 -0300

    Model em List

---
+++ Asp20/Asp20/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp20.Models;
+
+namespace Asp20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Cliente> lista_Clientes = new List<Cliente>()
+            {
+                new Cliente()
+                {
+                    nome = "João",
+                    apelido = "Ribeiro"
+                },
+                new Cliente()
+                {
+                    nome = "Ana",
+                    apelido = "Silva"
+                },
+                new Cliente()
+                {
+                    nome = "Carlos",
+                    apelido = "Oliveira"
+                }
+            };
+
+            return View(lista_Clientes);
+        }
+    }
+}
---
+++ Asp20/Asp20/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp20.Models
+{
+    public class Cliente
+    {
+        public string nome { get; set; }
+        public string apelido { get; set; }
+    }
+}
