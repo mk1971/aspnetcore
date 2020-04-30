f87bd0d621fc87c7ba63d2f3470383890e3a3b9f
mk1971 <wagner.maki@hotmail.com>
2020-04-30 12:26:50 -0300

    simplificando o entendimento sobre Models

---
+++ Asp19/Asp19/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp19.Models;
+
+namespace Asp19.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Cliente novo_Cliente = new Cliente();
+            novo_Cliente.nome = "João";
+            novo_Cliente.apelido = "Ribeiro";
+
+            return View(novo_Cliente);
+        }
+    }
+}
---
+++ Asp19/Asp19/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp19.Models
+{
+    public class Cliente
+    {
+        public string nome { get; set; }
+        public string apelido { get; set; }
+    }
+}
