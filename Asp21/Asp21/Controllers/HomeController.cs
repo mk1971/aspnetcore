b27bf34332e6713a67bd557fb98642ea875e40ce
mk1971 <wagner.maki@hotmail.com>
2020-05-04 15:18:06 -0300

    Exemplo de navegacao por detalhe de cliente

---
+++ Asp21/Asp21/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp21.Models;
+
+namespace Asp21.Controllers
+{
+    public class HomeController : Controller
+    {
+        //definicao de lista de dados
+        List<Cliente> lista_clientes;
+
+        public HomeController()
+        {
+            //controlador da classe HomeController
+            lista_clientes = new List<Cliente>()
+            {
+                new Cliente(){ id = 0, Nome = "João", Apelido = "Ribeiro"},
+                new Cliente(){ id = 1, Nome = "Ana", Apelido = "Silva"},
+                new Cliente(){ id = 2, Nome = "Carlos", Apelido = "Oliveira"}
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(lista_clientes);
+        }
+
+        public ViewResult detalhes(int id)
+        {
+            return View(lista_clientes[id]);
+        }
+    }
+}
---
+++ Asp21/Asp21/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp21.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+    }
+}
