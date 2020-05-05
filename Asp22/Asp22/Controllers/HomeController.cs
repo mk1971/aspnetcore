706345003d86cf28c911443e59817c48952e45e6
mk1971 <wagner.maki@hotmail.com>
2020-05-05 10:25:12 -0300

    formulario e model

---
+++ Asp22/Asp22/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp22.Models;
+
+namespace Asp22.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // view de entrada que apresenta um titulo, a lista de clientes ou a mensagem indicando que nao existem cliente
+            return View(Dados.Lista_Clientes);
+        }
+
+        public IActionResult novo_Cliente()
+        {
+            // apresenta o formulario para adicionar novo cliente
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult novo_Cliente(Cliente item)
+        {
+            // adicionar o novo cliente a colecao
+            Dados.Lista_Clientes.Add(item);
+
+            return RedirectToAction("index");
+        }
+    }
+}
