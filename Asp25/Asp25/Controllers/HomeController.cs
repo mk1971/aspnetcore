4325e90d05ab7ec6c4f71f9b1efbd51f51a3f291
mk1971 <wagner.maki@hotmail.com>
2020-05-06 14:52:35 -0300

    Editar um registro da colecao de dados

---
+++ Asp25/Asp25/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp25.Models;
+
+namespace Asp25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Dados.Todos_os_Dados());
+        }
+
+        public IActionResult Novo_Cliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Novo_Cliente(Cliente item)
+        {
+            Dados.AdicionarCliente(item);
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            Dados.EliminarClientes(id);
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            // apresentar a view de edicao
+            return View(Dados.Dados_Cliente(id));
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Cliente c)
+        {
+            // alterar os dados do cliente
+            Dados.Editar_Cliente(c);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Asp25/Asp25/Models/Dados.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp25.Models
+{
+    public static class Dados
+    {
+        private static List<Cliente> Lista_Clientes = new List<Cliente>();
+
+        public static IEnumerable<Cliente> Todos_os_Dados()
+        {
+            return Lista_Clientes;
+        }
+
+        public static void AdicionarCliente(Cliente item)
+        {
+            item.id = 0;
+            if(Lista_Clientes.Count != 0)
+            {
+                item.id = Lista_Clientes.Last<Cliente>().id + 1;
+            }
+
+            Lista_Clientes.Add(item);
+        }
+
+        public static void EliminarClientes(int id)
+        {
+            var item = Lista_Clientes.First<Cliente>(i => i.id == id);
+            Lista_Clientes.Remove(item);
+        }
+
+        public static Cliente Dados_Cliente(int id)
+        {
+            var cliente = Lista_Clientes.Where(i => i.id == id).FirstOrDefault();
+            return cliente;
+        }
+
+        public static void Editar_Cliente(Cliente c)
+        {
+            Lista_Clientes.Where(i => i.id == c.id).First().Nome = c.Nome;
+            Lista_Clientes.Where(i => i.id == c.id).First().Telefone = c.Telefone;
+        }
+    }
+}
