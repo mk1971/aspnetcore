6f624bce4d7180bb35dbbd9c1723cae3fb3a9946
mk1971 <wagner.maki@hotmail.com>
2020-05-06 13:15:09 -0300

    alterando a logica de funcionamento do model

---
+++ Asp23/Asp23/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Asp23.Models; 
+
+namespace Asp23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // view de entrada que apresenta um titulo, a lista de clientes ou a mensagem indicando que nao existem cliente
+            return View(Dados.Todos_os_Dados());
+        }
+
+        public IActionResult Novo_Cliente()
+        {
+            // apresenta o formulario para adicionar novo cliente
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Novo_Cliente(Cliente item)
+        {
+            // pede ao model que adicione o novo cliente caso seja possivel
+            Dados.AdicionarCliente(item);
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Asp23/Asp23/Models/Dados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp23.Models
+{
+    public static class Dados
+    {
+        private static List<Cliente> Lista_Clientes = new List<Cliente>();
+
+        //public static IEnumerable<Cliente> Todos_os_Dados
+        //{
+        //    get { return Lista_Clientes; }
+        //}
+
+        public static IEnumerable<Cliente> Todos_os_Dados()
+        {
+            return Lista_Clientes;
+        }
+
+        public static void AdicionarCliente(Cliente item)
+        {
+            Lista_Clientes.Add(item);
+        }
+    }
+}
