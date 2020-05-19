d95fe8f0c1e9d861193016f622b68718433951bc
mk1971 <wagner.maki@hotmail.com>
2020-05-19 15:19:34 -0300

    Sintaxe Razor 2 de 2

---
+++ Asp28/Asp28/Classes/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp28.Classes
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+    }
+}
