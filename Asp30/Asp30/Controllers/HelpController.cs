8d185fffc23b0c6405844313466ce0464a7572db
mk1971 <wagner.maki@hotmail.com>
2020-05-25 10:01:38 -0300

    Anchor TagHelper

---
+++ Asp30/Asp30/Controllers/HelpController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp30.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Metodo1()
+        {
+            return "Estou no método 1 do help controller";
+        }
+    }
+}
---
+++ Asp30/Asp30/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp30.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Metodo1()
+        {
+            return "Estou no método 1 do home controller";
+        }
+        public string Metodo2(int id)
+        {
+            return "Estou no método 2 do home controller e o valor de ID = " + id;
+        }
+
+        [Route("Metodo3/{nome}")]
+        public string Metodo3(string nome)
+        {
+            return "Estou no método 3 do home controller e o meu nome é " + nome;
+        }
+    }
+}
