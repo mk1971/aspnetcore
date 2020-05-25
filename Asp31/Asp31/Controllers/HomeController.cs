72181912862e3520352873bb43bae564d41d3276
mk1971 <wagner.maki@hotmail.com>
2020-05-25 10:57:01 -0300

    Form TagHelper

---
+++ Asp31/Asp31/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp31.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("formulario");
+        }
+
+        public string processar()
+        {
+            return "Estou no processar";
+        }
+
+        public string processar2()
+        {
+            return "Estou no processar2";
+        }
+
+        [Route("Home/processar_Final", Name = "teste")]
+        public string processar_Final()
+        {
+            return "Estou no processar Final!";
+        }
+    }
+}
---
+++ Asp31/Asp31/Controllers/OutroController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp31.Controllers
+{
+    public class OutroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string processar()
+        {
+            return "Estou no processar. (OutroController)";
+        }
+    }
+}
