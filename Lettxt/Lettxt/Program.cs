0b8abd88c3c1c1dd3d9f0a876714b02d9a0298f9
mk1971 <wagner.maki@hotmail.com>
2020-05-28 15:16:11 -0300

    Ler arquivo texto

---
+++ Lettxt/Lettxt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lettxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cria um leitor e abre o arquivo
+            TextReader tr = new StreamReader("textfilereader.txt");
+
+            //le uma linha de texto
+            Console.WriteLine(tr.ReadLine());
+            Console.ReadKey();
+
+            //fecha o stream
+            tr.Close();
+        }
+    }
+}
