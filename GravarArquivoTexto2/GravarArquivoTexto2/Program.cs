bb3d391f94f0c3bf0a37ae1197fb80a259eaf704
mk1971 <wagner.maki@hotmail.com>
2020-05-28 15:30:38 -0300

    Ler arquivo texto

---
+++ GravarArquivoTexto2/GravarArquivoTexto2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GravarArquivoTexto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivo = @"C:\NetCore\aspnetcore\GravarArquivoTexto2\GravarArquivoTexto2.txt";
+            if (File.Exists(arquivo))
+            {
+                try
+                {
+                    using(StreamReader sr = new StreamReader(arquivo))
+                    {
+                        String linha;
+
+                        //le linha por linha
+                        while ((linha = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(linha);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("O arquivo " + arquivo + " não foi localizado!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
