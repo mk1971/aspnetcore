9014f148286a7402a5f931fa153c162c72cc77ef
mk1971 <wagner.maki@hotmail.com>
2020-05-28 11:18:13 -0300

    Varios ambientes

---
+++ AspNetCore_Varios_Ambientes/AspNetCore_Varios_Ambientes/Classes/EnvironmentExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_Varios_Ambientes.Classes
+{
+    public static class EnvironmentExtension
+    {
+        [Obsolete]
+        public static bool IsTest(this IHostingEnvironment env)
+        {
+            return env.IsEnvironment(StandardEnvironment.Test);
+        }
+
+        public static bool IsTestExt(this IHostingEnvironment env) => env.IsEnvironment(StandardEnvironment.TestExt);
+
+    }
+}
---
+++ AspNetCore_Varios_Ambientes/AspNetCore_Varios_Ambientes/Classes/StandardEnvironment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_Varios_Ambientes.Classes
+{
+    public static class StandardEnvironment
+    {
+        public const string Development = "Development";
+        public const string Test = "Test";
+        public const string Staging = "Staging";
+        public const string Production = "Production";
+    }
+}
