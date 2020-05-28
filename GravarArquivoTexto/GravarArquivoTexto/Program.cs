cf485b0ad683017a25e8261422f6d63862ac36af
mk1971 <wagner.maki@hotmail.com>
2020-05-28 15:21:34 -0300

    GravarArquivoTexto

---
+++ GravarArquivoTexto/GravarArquivoTexto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GravarArquivoTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cria um escrito e abre o arquivo
+            TextWriter tw = new StreamWriter("GravarArquivoTexto.txt");
+
+            //escreve uma linha de texto no arquivo
+            tw.WriteLine(DateTime.Now);
+
+            //fecha o stream
+            tw.Close();
+        }
+    }
+}
