aa1e1e60cef4d1dd711b76e398e69e803b4a8e81
mk1971 <wagner.maki@hotmail.com>
2020-06-02 15:23:50 -0300

    Calcula a maior idade

---
+++ pooMaiorIdade/pooMaiorIdade/Pessoa.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMaiorIdade
+{
+    public class Pessoa
+    {
+        //construtores
+        public Pessoa()
+        {
+            this.Nome = "";
+            this.Idade = 0;
+        }
+
+        //sobrecarga
+        public Pessoa(String nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        //propriedades
+        private String nome;
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        private int idade;
+
+        public int Idade 
+        {
+            get
+            {
+                return idade;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    this.idade = 0;
+                }
+            }
+        }
+
+        //metodos
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+            Console.WriteLine("Idade: " + this.Idade);
+        }
+    }
+}
---
+++ pooMaiorIdade/pooMaiorIdade/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pessoa p1 = new Pessoa();
+            //Pessoa p2 = new Pessoa();
+            //Pessoa p3 = new Pessoa();
+
+            Pessoa p1 ;
+            Pessoa p2 ;
+            Pessoa p3 ;
+            Console.WriteLine("Determina qual a pessoa é a mais velha");
+            //Leitura de dados da 1a pessoa
+            Console.Write("Nome da 1a pessoa: ");
+            p1 = new Pessoa();
+            p1.Nome = Console.ReadLine();
+
+            Console.Write("Idade da 1a pessoa: ");
+            p1.Idade = Convert.ToInt32(Console.ReadLine());
+
+            //Leitura de dados da 2a pessoa
+            Console.Write("Nome da 2a pessoa: ");
+            p2 = new Pessoa();
+            p2.Nome = Console.ReadLine();
+
+            Console.Write("Idade da 2a pessoa: ");
+            p2.Idade = Convert.ToInt32(Console.ReadLine());
+
+            //Leitura de dados da 3a pessoa
+            Console.Write("Nome da 3a pessoa: ");
+            //p1 = new Pessoa();
+            //p1.Nome = Console.ReadLine();
+            String nome = Console.ReadLine();
+            //Console.WriteLine("Idade da 3a pessoa: ");
+            Console.Write("Idade da 3a pessoa: ");
+            //p1.Idade = Convert.ToInt32(Console.ReadLine());
+            int idade = Convert.ToInt32(Console.ReadLine());
+            p3 = new Pessoa(nome, idade);
+
+            //Logica
+            if ((p1.Idade > p2.Idade) && (p1.Idade > p3.Idade))
+            {
+                Console.WriteLine("A pessoa mais velha é: ");
+                p1.ExibirDados();
+            }
+            else
+            {
+                if ((p2.Idade > p3.Idade) && (p2.Idade > p1.Idade))
+                {
+                    Console.WriteLine("A pessoa mais velha é: ");
+                    p2.ExibirDados();
+                }
+                else
+                {
+                    if ((p3.Idade > p1.Idade) && (p3.Idade > p2.Idade))
+                    {
+                        Console.WriteLine("A pessoa mais velha é: ");
+                        p3.ExibirDados();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Todas as pessoas possuem a mesma idade.");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
