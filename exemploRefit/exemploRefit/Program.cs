26aee687b7e5fb158b8cc63cb616c77aa50e506d
mk1971 <wagner.maki@hotmail.com>
2020-06-06 12:15:07 -0300

    consumindo API REST com Refit (JSON)

---
+++ exemploRefit/exemploRefit/Program.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace exemploRefit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações ao CEP: " + cepInformado);
+
+                var address = await cepClient.GetAdressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}\nCidade{address.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+        }
+    }
+}
